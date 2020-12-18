5bcbbd5f0a81c26b4e3f0f46c2aaedda882c028c
Nigel Brown <pnigelbrown@gmail.com>
2020-12-18 11:19:27 -0600

    flip to vertical and add download button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./styles.css";
 import data from "./omni.csv";
 
+const saveSvgAsPng = require("save-svg-as-png");
+
+const imageOptions = {
+  scale: 5,
+  encoderOptions: 1,
+  backgroundColor: "white"
+};
+
 const parsedData = data
   .split("\n") // split string to lines
   .map((e) => e.trim()) // remove white spaces for each line
@@ -17,18 +25,28 @@ for (const value of parsedData) {
 }
 
 export default function App() {
+  this.handleClick = () => {
+    saveSvgAsPng.saveSvgAsPng(
+      document.getElementById("chart"),
+      "chart.png",
+      imageOptions
+    );
+  };
+
   return (
     <div className="App">
       {/* viewBox="x y width height" -- width can be anything, height must be height of boxes. */}
       {/* width == height -> square */}
       <svg
+        id="chart"
         viewBox={`0 0 ${boxes.length} ${boxes.length}`}
         xmlns="http://www.w3.org/2000/svg"
       >
         {boxes.map((color, i) => (
-          <rect x="0" y={i} width="100%" height="1" fill={color} />
+          <rect x={i} y="0" width="100%" height="100%" fill={color} />
         ))}
       </svg>
+      <button onClick={this.handleClick}>Download Image</button>
     </div>
   );
 }
