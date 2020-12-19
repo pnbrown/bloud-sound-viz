fd65f986195ca6f2346c9215a3af412e06c86ff5
Nigel Brown <pnigelbrown@gmail.com>
2020-12-18 18:35:10 -0600

    Adding other files and inverting saturation function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import data from "./omni.csv";
+import data from "./forward.csv";
 
 const saveSvgAsPng = require("save-svg-as-png");
 
@@ -19,9 +19,28 @@ let boxes = [];
 
 for (const value of parsedData) {
   let hue = (value[0] / 22000) * 360;
-  let saturation = Math.abs(value[1]);
+  // need more granularity in the saturation. 10 dB should be much more significant
+  let saturation = 100 - Math.abs(value[1]);
   // console.log(hue, saturation);
   boxes.push(`hsl(${hue}, ${saturation}%, 50%`);
+}
+
+class ReactLogo extends React.Component {
+  render() {
+    return (
+      // {/* viewBox="x y width height" -- width can be anything, height must be height of boxes. */}
+      // {/* width == height -> square */}
+      <svg
+        id="chart"
+        viewBox={`0 0 1023 ${boxes.length}`}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        {boxes.map((color, i) => (
+          <rect x={i} y="0" width="100%" height="100%" fill={color} />
+        ))}
+      </svg>
+    );
+  }
 }
 
 export default function App() {
@@ -32,21 +51,12 @@ export default function App() {
       imageOptions
     );
   };
-
   return (
     <div className="App">
-      {/* viewBox="x y width height" -- width can be anything, height must be height of boxes. */}
-      {/* width == height -> square */}
-      <svg
-        id="chart"
-        viewBox={`0 0 ${boxes.length} ${boxes.length}`}
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {boxes.map((color, i) => (
-          <rect x={i} y="0" width="100%" height="100%" fill={color} />
-        ))}
-      </svg>
-      <button onClick={this.handleClick}>Download Image</button>
+      <header className="App-header">
+        <ReactLogo />
+        <button onClick={this.handleClick}>Download Image</button>
+      </header>
     </div>
   );
 }
